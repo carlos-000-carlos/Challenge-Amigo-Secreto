c6e3574bf207e3c1db7e9e47d8182f5120fec405
Carlos_Cabral <cabral.events@gmail.com>
2025-08-06 18:36:39 -0300

    criação do array amigos e da função que agrega nome dos amigos

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-// Espera que o DOM esteja totalmente carregado para garantir que o elemento existe
-    document.addEventListener('DOMContentLoaded', function() {
-      // Obtém o elemento do campo de input pelo ID
-      const campoNome = document.getElementById('amigo');
+let nomeDoAmigo = [];
+console.log(nomeDoAmigo);
 
-      // Coloca o foco no campo, o que também ativa o cursor
-      if (campoNome) { // Verifica se o elemento foi encontrado
-        campoNome.focus();
-      }
-    });
+cursorNoCampoNomeDoAmigo();
+
+
+function cursorNoCampoNomeDoAmigo() {
+
+  // Espera que o DOM esteja totalmente carregado para garantir que o elemento existe
+  document.addEventListener('DOMContentLoaded', function() {
+    // Obtém o elemento do campo de input pelo ID
+    const campoNome = document.getElementById('amigo');
+    
+    // Coloca o foco no campo, o que também ativa o cursor
+    if (campoNome) { // Verifica se o elemento foi encontrado
+      campoNome.focus();
+    }
+  });
+}
+
+function adicionarAmigo() {
+  let nome = document.querySelector('input');
+  if (nome.value == '') {
+    alert('Por favor, digite um nome.');
+ } else {
+    if (nomeDoAmigo.includes(nome.value)) {
+      alert('Por favor, digite outro nome, pois este nome já está na lista de amigos.');
+      limparCampo();
+    }
+    else {
+        nomeDoAmigo.push(nome.value);
+        limparCampo();
+    }
+    console.log(nomeDoAmigo);
+    }
+  }
+
+function limparCampo() {
+    cursorNoCampoNomeDoAmigo();
+    adicionar = document.querySelector('input');
+    adicionar.value = '';
+}
